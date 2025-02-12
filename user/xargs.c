f488995cb4bb5b3e9244893828ffe821e989b97d
Ken Hwang <ken.r.hwang.com>
2025-02-12 20:06:34 +0800

    Lab utils: xargs

---
+++ user/xargs.c
@@ -0,0 +1,47 @@
+#include "kernel/param.h"
+#include "kernel/types.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  char *cmd;
+  if (argc < 2) {
+    cmd = "echo";
+  } else {
+    cmd = argv[1];
+  }
+
+  char buf[512];
+  char *p = buf;
+  char c;
+
+  while (((read(0, &c, sizeof(char))) > 0))
+  {
+    if (c != '\n') {
+      *p++ = c;
+    } else {
+      *p = '\0';
+      p = buf;
+
+      if(fork() == 0) {
+        char *args[MAXARG] = {[0] = cmd};
+
+        int i;
+        for (i = 2; i < (argc); i++) {
+          args[i-1] = argv[i];
+        }
+
+        args[--i] = buf;
+
+        exec(cmd, args);
+        fprintf(2, "exec %s failed\n", cmd);
+      } else {
+        wait(0);
+        memset(buf, 0, sizeof(buf));
+      }
+    }
+  }
+
+  exit(0);
+}
